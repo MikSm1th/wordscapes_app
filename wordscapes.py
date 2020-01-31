3c38e5558282973088f9d09af7028886863f7991
Michael Smith <thiscuriousquest@gmail.com>
2020-01-30 21:00:38 -0500

    2020-01-30

--- wordscapes.py
+++ wordscapes.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 
+import streamlit as st
 import itertools
 
+def main():
+    st.header("A simple wordscapes app")
+    st.subheader("A demo on how to use Streamlit")
+    
+
+if __name__ == "__main__":
+    main()
+
+#def run_wordscapes():
 with open("words.txt") as dict_file:
     valid_words = [''.join(line.strip().split()).lower() for line in dict_file if len(line) <= 12]
 
-letters = input('What are your letters?:')
+#letters = input('What are your letters?:')
+letters = st.text_input(label='What are your letters?', value='')
 for word_length in range(3, len(letters) + 1):
     words = set()
     for possible_word_tuple in itertools.permutations(letters, word_length):
@@ -13,6 +24,6 @@ for word_length in range(3, len(letters) + 1):
         if possible_word_string in valid_words:
             words.add(possible_word_string)
 
-    print("Matching {} letter words:".format(word_length))
+    st.write("Matching {} letter words:".format(word_length))
     for word in words:
-        print("\t {}".format(word))
+        st.write("\t {}".format(word))
